82fe8798a7f7d13dfabbc8bd50e2ff1bda762261
snipeet03 <navneetlonare77@gmail.com>
2026-02-07 17:59:34 +0530

    fix: correct import path for Img component in HeroBanner

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import useFetch from '../../../hooks/useFetch';
 import { useSelector } from 'react-redux';
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
-import Img from "../../../../components/lazyLoadImage/Img";
+import Img from "../../../components/lazyLoadImage/Img";
 
 
 const HeroBanner = () => {
